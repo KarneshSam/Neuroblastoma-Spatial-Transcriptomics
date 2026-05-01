a17428bc61c32d9d50252acaa04c4f14dcf0954e
Karnesh Sampath <inf-41-2025@bioinf-serv2.cob.lu.se>
2026-05-01 16:48:11 +0200

    setup the required packages for the workflow

---
+++ Scripts/00_setup.R
@@ -0,0 +1,14 @@
+# 00_setup.R
+# ─────────────────────────────────────────────────────────────────
+
+library(Seurat)
+library(ggplot2)
+library(ggrepel)
+library(patchwork)
+library(dplyr)
+library(leiden)
+library(Banksy)
+library(pheatmap)
+library(SeuratWrappers)
+library(clustree)
+library(Matrix)
